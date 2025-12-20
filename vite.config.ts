abe12ee4de3df39416d4a2303ee52866972fef82
Sanskriti Agrawal <sanskritiagrawal@Sanskritis-MacBook-Air.local>
2025-12-20 17:27:00 +0530

    misc changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/portfolio-builder/",
+  base: mode === "production" ? "/portfolio-builder/" : "/",
   server: {
     host: "::",
     port: 8080,
